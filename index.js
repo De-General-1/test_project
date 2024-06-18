f0dc97b6442cb7e6afa7587b43a0bb6237454d88
De-General-1 <82713806+De-General-1@users.noreply.github.com>
2024-06-18 23:21:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT
 export const connect = () =>{
-<<<<<<< HEAD
     mongoose.connect(process.env.MONGO_URL, {
-=======
-    mongoose.connect(process.env.MONGO_URL, {
->>>>>>> e3834320d9bb7309f58e8f87466678db6db93ffb
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(()=>{
